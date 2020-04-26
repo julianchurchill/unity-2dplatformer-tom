4b3e888cea415689d0164dec6d22c5e90c57db16
Julian Churchill <chewielouie03@gmail.com>
2020-04-26 17:03:52 +0100

    Added time manager and Home button when time runs out

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public static TimeManager instance;
+    public TextMeshProUGUI text;
+    TimeSpan time = TimeSpan.FromMinutes(3);
+    GameObject[] finishObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+
+        finishObjects = GameObject.FindGameObjectsWithTag("ShowOnFinish");
+        Time.timeScale = 1;
+        foreach(GameObject g in finishObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    public void Update()
+    {
+        var timeChange = TimeSpan.FromSeconds(Time.deltaTime);
+        time -= timeChange;
+        text.text = time.ToString(@"mm\:ss");
+
+        if(time <= TimeSpan.FromSeconds(0))
+        {
+            Time.timeScale = 0;
+            foreach(GameObject g in finishObjects)
+            {
+                g.SetActive(true);
+            }
+        }
+    }
+}
